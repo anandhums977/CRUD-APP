e1a3ad8a040f44794cab12e93989c58988c750a5
Fredy Daniel <fredyd900@gmail.com>
2023-08-03 23:35:06 +0200

    router-navigation-after-delete-removed-and-post-card-single-view-crefactored

--- src/app/post/components/post-card/post-card.component.ts
+++ src/app/post/components/post-card/post-card.component.ts
@@ -1,40 +1,34 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 import { ToastrService } from 'ngx-toastr';
 import { PostService } from 'src/app/services/post/post.service';
 import { IPost, IPostsData } from 'src/app/shared/interface/post/post';
-import { Router } from '@angular/router';
+import { OnSameUrlNavigation, Router } from '@angular/router';
 import { finalize } from 'rxjs/operators';
+import { BehaviorSubject, Observable } from 'rxjs';
 @Component({
   selector: 'app-post-card',
   templateUrl: './post-card.component.html',
-  styleUrls: ['./post-card.component.css']
+  styleUrls: ['./post-card.component.css'],
 })
 export class PostCardComponent implements OnInit {
-
   @Input() postDetails!: IPost;
   showFullContent = false;
 
-  constructor(private postService: PostService, private toastr: ToastrService, private router: Router) { }
+  constructor(
+    private postService: PostService,
+    private toastr: ToastrService
+  ) {}
 
-  ngOnInit() {
-
-
-
-  };
-
-
+  ngOnInit() {}
 
   deletePost(id: string): void {
-    
-    this.postService.deletePost(id)
+    this.postService
+      .deletePost(id)
       .pipe(
         finalize(() => {
           this.toastr.success('Success', 'Post Deleted Successfully!');
-          this.router.navigate(['post/add']);
         })
       )
       .subscribe();
   }
-  
-  
 }
