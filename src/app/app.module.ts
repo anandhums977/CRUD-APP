841b3bb358608a2f1b3f152572d5f4b8cbd30e79
anandhums977 <anandhums635@gmsil.com>
2023-10-24 23:03:12 +0530

    deployment setup

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,4 @@
-import { NgModule } from '@angular/core';
+import { NgModule,CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
 import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
@@ -19,6 +19,7 @@ import { SharedModule } from './shared/shared.module';
     NgxSpinnerModule.forRoot(),
     ToastrModule.forRoot(),
   ],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA],
   providers: [],
   bootstrap: [AppComponent],
 })
